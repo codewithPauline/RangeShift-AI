80244dcfd9d2cc516a62d069c5e09e47dd1724df
Pauline Owusu-Ansah <paulineowusu653@gmail.com>
2026-09-08 16:48:28 -0400

    Expose v0.3 raster API

--- src/rangeshift/__init__.py
+++ src/rangeshift/__init__.py
@@ -2,6 +2,12 @@
 
 from .geospatial import ProjectedBlockResult, assign_projected_blocks, estimate_local_utm_epsg
 from .model import TrainingResult, train_habitat_model
+from .raster import (
+    RasterPredictionResult,
+    RasterStack,
+    parse_layer_specs,
+    predict_suitability_raster,
+)
 from .spatial import (
     SpatialComparisonResult,
     SpatialEvaluationResult,
@@ -13,6 +19,8 @@ from .spatial_cv import SpatialCVResult, cross_validate_spatial_blocks, spatial_
 
 __all__ = [
     "ProjectedBlockResult",
+    "RasterPredictionResult",
+    "RasterStack",
     "SpatialCVResult",
     "SpatialComparisonResult",
     "SpatialEvaluationResult",
@@ -23,7 +31,9 @@ __all__ = [
     "cross_validate_spatial_blocks",
     "estimate_local_utm_epsg",
     "evaluate_spatial_holdout",
+    "parse_layer_specs",
+    "predict_suitability_raster",
     "spatial_cross_validate",
     "train_habitat_model",
 ]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
