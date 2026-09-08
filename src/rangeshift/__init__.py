7d2a53a65743b6fe782616c892386dd06533bf22
Pauline Owusu-Ansah <paulineowusu653@gmail.com>
2026-09-08 16:22:03 -0400

    Add RangeShift package initializer

---
+++ src/rangeshift/__init__.py
@@ -0,0 +1,6 @@
+"""RangeShift AI: machine-learning tools for habitat suitability and range-shift analysis."""
+
+from .model import TrainingResult, train_habitat_model
+
+__all__ = ["TrainingResult", "train_habitat_model"]
+__version__ = "0.1.0"
