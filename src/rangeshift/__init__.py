74b491b2c292b6bb21f1eabc087e0891ba9b6bcd
Pauline Owusu-Ansah <paulineowusu653@gmail.com>
2026-09-08 20:28:01 -0400

    Expose range-shift map renderer

--- src/rangeshift/__init__.py
+++ src/rangeshift/__init__.py
@@ -17,7 +17,7 @@ from .spatial import (
     evaluate_spatial_holdout,
 )
 from .spatial_cv import SpatialCVResult, cross_validate_spatial_blocks, spatial_cross_validate
-from .visualization import plot_suitability_map
+from .visualization import plot_range_shift_map, plot_suitability_map
 
 __all__ = [
     "ProjectedBlockResult",
@@ -36,6 +36,7 @@ __all__ = [
     "estimate_local_utm_epsg",
     "evaluate_spatial_holdout",
     "parse_layer_specs",
+    "plot_range_shift_map",
     "plot_suitability_map",
     "predict_suitability_raster",
     "spatial_cross_validate",
