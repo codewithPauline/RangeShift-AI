cfc24ef09e981ce71a1009c1521ea15ec9d1610f
Pauline Owusu-Ansah <paulineowusu653@gmail.com>
2026-09-08 21:40:15 -0400

    Remove unused background import

--- src/rangeshift/background.py
+++ src/rangeshift/background.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from math import asin, cos, radians, sin, sqrt
+from math import asin, cos, radians, sin
 
 import numpy as np
 import pandas as pd
